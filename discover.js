006818ce03cb86336bf3262180a6fc5045eb9df8
mlarsen764 <mlarsen764@gmail.com>
2024-02-27 11:23:25 -0700

    display username entered at login on the discover page

--- discover.js
+++ discover.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var collapsibleHeaders = document.querySelectorAll('.collapsible-header');
-    collapsibleHeaders.forEach(function(header) {
-      header.addEventListener('click', function() {
-        this.classList.toggle('active');
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
+  var collapsibleHeaders = document.querySelectorAll('.collapsible-header');
+  collapsibleHeaders.forEach(function(header) {
+    header.addEventListener('click', function() {
+      this.classList.toggle('active');
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
           content.style.maxHeight = null;
-        } else {
+      } else {
           content.style.maxHeight = content.scrollHeight + "px";
-        }
-      });
+      }
     });
   });
+  function getUserName() {
+    return localStorage.getItem('userName') ?? 'Guest';
+  }
+  document.getElementById('userNameDisplay').textContent = getUserName();
+});
+
