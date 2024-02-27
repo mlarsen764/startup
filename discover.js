8922c830effe0e07bfe6d991ba58ab18ece7b77d
mlarsen764 <mlarsen764@gmail.com>
2024-02-27 11:00:11 -0700

    add simon, move discover javascript to separate file

---
+++ discover.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var collapsibleHeaders = document.querySelectorAll('.collapsible-header');
+    collapsibleHeaders.forEach(function(header) {
+      header.addEventListener('click', function() {
+        this.classList.toggle('active');
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    });
+  });
