65e56c692d20a6def6edd026e1297d594b04dc2b
mlarsen764 <mlarsen764@gmail.com>
2024-02-28 10:17:13 -0700

    allow user to press enter to login

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("login").addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            login();
+        }
+    });
+});
+
 function login() {
     const nameEl = document.querySelector("#name");
     localStorage.setItem("userName", nameEl.value);
