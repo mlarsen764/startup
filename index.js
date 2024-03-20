eaf56a51b7519269d5b7a3b62fbdd0543141a262
mlarsen764 <mlarsen764@gmail.com>
2024-03-20 14:36:56 -0600

    add cookieParser and bcrypt

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
 const express = require('express');
 const app = express();
+const DB = require('./database.js')
+
+const authCookieName = 'token';
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
 
 // Serve up the frontend static content hosting
 app.use(express.static('public'));
