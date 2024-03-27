469eedc5fbe8a3cc13bb45c400dbe08334d25501
mlarsen764 <mlarsen764@gmail.com>
2024-03-27 12:16:26 -0600

    adjust server setup to use peerProxy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const express = require('express');
 const app = express();
 const DB = require('./database.js')
+const { peerProxy } = require('./peerProxy.js')
 
 const authCookieName = 'token';
 
@@ -151,6 +152,8 @@ function setAuthCookie(res, authToken) {
     });
 }
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const httpService = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
+  
+peerProxy(httpService);
